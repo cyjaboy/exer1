00a5f423a14252f624dda8391d02739e0125485d
cyjaboy <143145076+cyjaboy@users.noreply.github.com>
2023-09-08 11:04:58 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -14,5 +14,26 @@ function displayDifference() {
   document.getElementById("differenceanswer").innerHTML = `${firstNum} - ${secondNum} equals to ${total}`;
 }
 
+
+function displayProduct() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML);
+  let secondNum = Number(document.getElementById('secondNum').innerHTML);
+
+  let total = firstNum * secondNum;
+  document.getElementById("productanswer").innerHTML = `${firstNum} * ${secondNum} equals to ${total}`;
+}
+
+function displayQuotient() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML);
+  let secondNum = Number(document.getElementById('secondNum').innerHTML);
+
+  let total = firstNum / secondNum;
+  document.getElementById("quotientanswer").innerHTML = `${firstNum} / ${secondNum} equals to ${total}`;
+}
+
+
 document.getElementById('sumButton').addEventListener("click", displaySum);
 document.getElementById('differenceButton').addEventListener("click", displayDifference);
+document.getElementById('prodcutButton').addEventListener("click", displayProduct);
+document.getElementById('quotientButton').addEventListener("click", displayQuotient);
+
