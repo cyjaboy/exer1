5e8029948e9addd4e4484bee9dd3fdd428fd0d19
cyjaboy <143145076+cyjaboy@users.noreply.github.com>
2023-09-12 10:15:09 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -46,6 +46,32 @@ function displayQuotient() {
   document.getElementById("quotientanswer").innerHTML = `${firstNum} / ${secondNum} equals to ${total}`;
 }
 
+// app.js
+
+// Generate and display random numbers
+function generateRandomNumbers() {
+  const firstNumElement = document.getElementById("firstNum");
+  const secondNumElement = document.getElementById("secondNum");
+
+  const randomFirstNum = getRandomNumber(1, 1000); // Adjust the range as needed
+  const randomSecondNum = getRandomNumber(1, 1000); // Adjust the range as needed
+
+  firstNumElement.textContent = randomFirstNum;
+  secondNumElement.textContent = randomSecondNum;
+}
+
+// Function to get a random number within a specified range
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Event listener for the Refresh button
+document.getElementById("refreshButton").addEventListener("click", generateRandomNumbers);
+
+// Initialize random numbers when the page loads
+generateRandomNumbers();
+
+// Rest of your JavaScript code...
 
 
 
